98f30ddda1d101f5ad3fd5e8b15ffa3f32992177
leptoquark <claudio.biancalana@gmail.com>
2020-11-13 13:13:39 +0100

    primo commit

---
+++ src/it/almaviva/cluster/test/Info.java
@@ -0,0 +1,58 @@
+package it.almaviva.cluster.test;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Info
+ */
+@WebServlet(description = "test info", urlPatterns = { "/info" })
+public class Info extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Info() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+          try {
+            RequestDispatcher view = request.getRequestDispatcher("info.jsp");
+            view.forward(request,response);
+                    
+        } finally {
+            out.close();
+        }
+        }
+        
+    }
+
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		processRequest(request,response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
